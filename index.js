c849284cccb77eeeb6cacb7861cbc8546f980df3
Hristo Yonkov <hristopturs@gmail.com>
2023-10-31 21:30:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+// TODO: Add aditional libraties, middlewares!
+
+
+app.get('/', (req, res) => {
+    res.json({message: 'Service operational'});
+    res.end();
+});
+
+
+
+async function startServer() {
+    // TODO: USE aditional libraties, middlewares!
+    app.listen('3030', () => console.log('Server operational on port: 3030!'));
+}
