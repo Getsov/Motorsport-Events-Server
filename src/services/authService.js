7cd91c8737b41caa6f0c332bc1618bafb5f6b714
Hristo Yonkov <hristopturs@gmail.com>
2023-11-01 23:04:28 +0200

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,10 +1,10 @@
-const User = require('./models/User');
+const User = require('../models/User');
 
-async function register() {
+async function registerUser() {
     const user = await User.create({ username: 'Michael', email: 'Shumaher' });
     return user;
 };
 
 module.exports = {
-    register
+    registerUser
 }
