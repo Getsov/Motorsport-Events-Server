591d1119b93a85450af7ea07feeb267ea5e5ba6c
Hristo Yonkov <hristopturs@gmail.com>
2023-11-01 22:23:01 +0200

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,20 @@
 
+const User = require('./models/User');
 
 const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.json({ message: 'Service operational' });
+});
+
+router.get('/auth', async (req, res) => {
+    try {
+        const user = await User.create({ username: 'Michael', email: 'Shumaher' });
+        res.status(201).json(user);
+        res.end();
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+module.exports = router;
