466a124c10e64db65b5d2f657edb8c9f7cecd35a
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-02 14:19:07 +0200

    add some comments

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,8 @@
 const authController = require('express').Router();
 const { registerUser } = require('../services/authService');
 
-// In Future Pavka! Check CORS for requests "req" authorization, in index.js wich is commented!!!
-// In other words, if you turn on cors in index.js, no file will be returned on the client!
+//TODO:  In Future Pavka! Check CORS for requests "req" authorization, in index.js wich is commented!!!
+//TODO: In other words, if you turn on cors in index.js, no file will be returned on the client!
 
 authController.get('/register', async (req, res) => {
     // TODO: Check for passwords and token..
