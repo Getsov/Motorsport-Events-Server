9fe6a8fccc1d4ed1be777f2d177a064a46330762
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-02 15:27:19 +0200

    add login path

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 const authController = require('express').Router();
-const { registerUser } = require('../services/authService');
+const { registerUser, loginUser } = require('../services/authService');
 
 //TODO:  In Future Pavka! Check CORS for requests "req" authorization, in index.js wich is commented!!!
 //TODO: In other words, if you turn on cors in index.js, no file will be returned on the client!
@@ -8,6 +8,18 @@ authController.get('/register', async (req, res) => {
     // TODO: Check for passwords and token..
     try {
         const user = await registerUser();
+        
+        res.status(200).json(user);
+        res.end();
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+authController.get('/login', async (req, res) => {
+    // TODO: Check for passwords and token..
+    try {
+        const user = await loginUser();
         
         res.status(200).json(user);
         res.end();
