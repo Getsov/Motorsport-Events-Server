f23c4f32868b132b728df7715815f0e9e5cc2c44
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-02 14:01:59 +0200

    add bcrypt and jws

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,10 +1,14 @@
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 async function registerUser() {
     const user = await User.create({ username: 'Michael', email: 'Shumaher' });
     return user;
 };
+
+
+
 
 module.exports = {
     registerUser
