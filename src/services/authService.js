104d0f579a639de5c741d7eed88ac7177b2cebc0
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-02 14:25:15 +0200

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,14 +2,35 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
+
+
+
 async function registerUser() {
+
+    //TODO: use next line for real app without hardcore the user
+    // async function registerUser(username, email, firstName, lastName) {
+
+    //TODO: use next lines for real app
+    // const existing = await User.findOne({ email });
+    // if(existing){
+    //     throw new Error ('Email is already taken!!!');
+    // } 
+
+
+
+    //TODO: remove hardcore user
     const user = await User.create({
         username: 'Scuderia',
         email: 'Shumaher@gmail.com',
         firstName: 'Michael',
         lastName: 'Schumaher'
     });
+
     return user;
+
+
+    //TODO: use next line for real development
+    return createToken(user)
 };
 
 function createToken(user) {
@@ -28,10 +49,10 @@ function createToken(user) {
     }
 };
 
-function parseToken(token){
-    try{
+function parseToken(token) {
+    try {
         return jwt.verify(token, secret)
-    } catch{error}{
+    } catch { error } {
         throw new Error('Invalid acces token!')
     }
 }
