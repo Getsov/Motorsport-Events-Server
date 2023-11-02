931ff6b7a56af37011b5abf615adb8544fdd7b67
Hristo Yonkov <hristopturs@gmail.com>
2023-11-02 16:37:34 +0200

    Update Event.js

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,18 +1,38 @@
 const { Schema, model } = require('mongoose');
 
 const eventSchema = new Schema({
-    eventName: { type: String, require: true, },
+    title: {
+        type: String,
+        require: true,
+        minlength: [5, "Title must be minimum 5 characters long!"],
+        maxlength: [30, "Title must be maximum 30 characters long!"],
+    },
+    description: {
+        type: String,
+        require: true,
+        minlength: [5, "Description must be minimum 5 characters long!"],
+        maxlength: [200, "Description must be maximum 30 characters long!"],
+    },
+    date: { type: Date, require: true, },
+    time: { 
+        type: String,
+        require: true,
+        minlength: [5, "Hour must be in format: 'hh:mm'!"],
+        maxlength: [5, "Hour must be in format: 'hh:mm'!"],
+    },
+    eventLocation: { type: String, require: true, },
+    isDeleted: { type: Boolean, default: false }
 });
 
-// eventSchema.index(
-//     { eventName: 1 },
-//     {
-//         collation: {
-//             locale: "en",
-//             strength: 2,
-//         },
-//     }
-// );
+eventSchema.index(
+    { title: 1 },
+    {
+        collation: {
+            locale: "en",
+            strength: 2,
+        },
+    }
+);
 
 const Event = model("Event", eventSchema);
 module.exports = Event;
