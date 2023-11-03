f276a97e755f31aa13745c86e163df90d39e85cc
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-03 20:09:47 +0200

    change names of authService to userService

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 const userController = require('express').Router();
-const { registerUser, loginUser } = require('../services/authService');
+const { registerUser, loginUser } = require('../services/userService');
 
 //TODO:  In Future Pavka! Check CORS for requests "req" authorization, in index.js wich is commented!!!
 //TODO: In other words, if you turn on cors in index.js, no file will be returned on the client!
