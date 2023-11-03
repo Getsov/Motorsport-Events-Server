2c41f31d3a16f51222fef550e267c24d342f9d9a
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-03 20:42:41 +0200

    Update Organization.js

--- src/models/Organization.js
+++ src/models/Organization.js
@@ -1,1 +1,5 @@
 const { Schema, model, Types: { ObjectId }, } = require('mongoose');
+const emailRegex = /^[a-zA-Z0-9._%+-]{3,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+// const emailRegex2 = /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/;
+
+
