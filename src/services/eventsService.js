6efe56214ed83617892aa5a7697fabf1a8211f06
Hristo Yonkov <hristopturs@gmail.com>
2023-11-03 13:33:15 +0200

    Update eventsService.js

--- src/services/eventsService.js
+++ src/services/eventsService.js
@@ -2,12 +2,27 @@ const Event = require('../models/Event');
 
 async function registerEvent() {
     // TODO: make more tests with different values!
-    const event = await Event.create({ 
+    const event = await Event.create({
         title: 'Race Fanatics',
-        description: 'ala bala',
-        date: '2023-12-31',
-        time: '23:59',
-        eventLocation: 'Nqkude si..'
+        shortDescription: 'ala bala',
+        longDescription: 'Lorem ipsum dolor sit amet.',
+        dates: [{
+            startDate: Date.now(),
+            startTime: '23:59',
+            endTime: '06:40'
+        }], // Eventually add another date!
+        imageUrl: 'https://cdn.akamai.steamstatic.com/steam/apps/1839940/capsule_616x353.jpg?t=1683123302',
+        contacts: {
+            coordinates: { lat: '', long: '' },
+            city: 'Hisarya',
+            address: '',
+            phone: '0123456789',
+            email: 'peter@abv.bg'
+        },
+        category: 'Off Road',
+        likedCount: 1,
+        winners: [{ name: 'Az' }],
+        creator: '6542c24b6102c6f4e79108fc'
     });
 
     return event;
