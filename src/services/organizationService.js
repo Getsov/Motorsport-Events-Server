42396150461be7a5d646b44ce0cab838f386f0e8
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-04 13:54:26 +0200

    Update organizationService.js

--- src/services/organizationService.js
+++ src/services/organizationService.js
@@ -6,13 +6,39 @@ const Organization = require('../models/Organization');
 const secret = 'q213fdfsddfasd231adfas12321kl';
 
 async function registerOrganization(){
+
+        /*
+    TODO: use next line for real app without hardcore the organization
+
+    async function registerUser(username, email, firstName, lastName, password) {
+
+        const existing = await User.findOne({ email });
+        if (existing) {
+            throw new Error('Email is already taken!!!');
+        }
+
+        const user = await User.create({
+            username,
+            email,
+            firstName,
+            lastName,
+            hashedPassword: await bcrypt.hash(password, 10)
+        });
+        return createToken(user)
+
+*/
+
+    // TODO: remove hardcore user & password
     let password = 123;
     const organization = await Organization.create({
         name: 'Bardhal',
         email: 'bardhal@gmail.com',
-        location: 'Sofiq',
+        address: 'Sofiq',
         manager: 'Ivan Ivanov',
         phone: '0899123123',
+        createdEvents: [{}],
+        isDeleted: false,
         hashedPassword: await bcrypt.hash(password, 10)
-    })
+    });
+    return organization;
 }
