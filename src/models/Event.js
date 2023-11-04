43fa0df233b38a354ceeb44b89cc28b49d3f503a
Hristo Yonkov <hristopturs@gmail.com>
2023-11-04 14:45:11 +0200

    add required property on string

--- src/models/Event.js
+++ src/models/Event.js
@@ -44,6 +44,7 @@ const eventSchema = new Schema({
     },
     imageUrl: {
         type: String,
+        required: true,
         validate: {
             validator: (value) => validString.test(value),
             message: "Invalid URL, must start with HTTP/HTTPS",
