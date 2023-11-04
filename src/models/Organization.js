a2f5f4c498d258f10caf0d6436c6c474eaf8fba8
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-04 13:29:49 +0200

    Update Organization.js

--- src/models/Organization.js
+++ src/models/Organization.js
@@ -3,3 +3,6 @@ const emailRegex = /^[a-zA-Z0-9._%+-]{3,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 // const emailRegex2 = /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/;
 
 
+constOrgnaizationSchema = new Schema({
+    
+})
