09d277503ad9448fa1739f2a6398a21822912d38
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-04 13:40:58 +0200

    Update organizationService.js

--- src/services/organizationService.js
+++ src/services/organizationService.js
@@ -3,4 +3,16 @@ const jwt = require('jsonwebtoken');
 const Organization = require('../models/Organization');
 
 //TODO: use env and change secret
-const secret = 'q213fdfsddfasd231adfas12321kl'
+const secret = 'q213fdfsddfasd231adfas12321kl';
+
+async function registerOrganization(){
+    let password = 123;
+    const organization = await Organization.create({
+        name: 'Bardhal',
+        email: 'bardhal@gmail.com',
+        location: 'Sofiq',
+        manager: 'Ivan Ivanov',
+        phone: '0899123123',
+        hashedPassword: await bcrypt.hash(password, 10)
+    })
+}
