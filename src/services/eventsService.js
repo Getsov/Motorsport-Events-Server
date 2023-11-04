972e0b5ac5c2430d25ec974e25688a511929478e
Hristo Yonkov <hristopturs@gmail.com>
2023-11-04 14:46:44 +0200

    tested required coordinates successfully

--- src/services/eventsService.js
+++ src/services/eventsService.js
@@ -13,14 +13,13 @@ async function registerEvent() {
         }], // Eventually add another date!
         imageUrl: 'https://cdn.akamai.steamstatic.com/steam/apps/1839940/capsule_616x353.jpg?t=1683123302',
         contacts: {
-            coordinates: { lat: '', long: '' },
+            coordinates: { lat: 'sda', long: 'sad' },
             city: 'Hisarya',
             address: 'Хайдут Генчо N1',
             phone: '0123456789',
             email: 'peter@abv.bg'
         },
         category: 'Off Road',
-        likedCount: 1,
         winners: [{ name: 'Az' }],
         creator: '6542c24b6102c6f4e79108fc'
     });
