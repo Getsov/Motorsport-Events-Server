d4c3f286b6d7645b5aefbe98b88052b29a7e905a
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-04 15:01:09 +0200

    Update Organization.js

--- src/models/Organization.js
+++ src/models/Organization.js
@@ -1,12 +1,38 @@
-const { Schema, model, Types: { ObjectId }, } = require('mongoose');
+const {
+  Schema,
+  model,
+  Types: { ObjectId },
+} = require("mongoose");
 const emailRegex = /^[a-zA-Z0-9._%+-]{3,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 // const emailRegex2 = /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/;
 
-
-constOrgnaizationSchema = new Schema({
-
+OrgnaizationSchema = new Schema({
+  name: { type: String, require: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (value) => emailRegex.test(value),
+      message: "Invalid email",
+    },
+  },
+  //TODO adress,
+  managerFirstName: { type: String, require: true },
+  managerLasttName: { type: String, require: true },
+  //TODO regex for phone, or minimum and maximum length
+  phone: { type: String, require: true },
+  city: { type: String, required: true },
+  address: { type: String, required: true },
+  createdEvents: { type: ObjectId, ref: "Event", required: true },
+  isDeleted: { type: Boolean, default: false },
 });
+/*
 
 
-const Organization = model("Organization", constOrgnaizationSchema);
+isDeleted,
+
+*/
+
+const Organization = model("Organization", OrgnaizationSchema);
 module.exports = Organization;
