8ed94fa58439a4a6462cdab4519fb88c40b3faa0
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-07 00:09:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ const initializeDatabase = () => mongoose.connect(dbConnectionString)
 async function startServer() {
     initializeDatabase();
     // TODO: USE aditional libraties, middlewares!
-    // app.use(cors);
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
     app.use(router);
     app.listen('3030', () => console.log('Server operational on port: 3030!'));
 };
