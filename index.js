74192eb06f1ab225ebfc8cbc273058c6c17c521d
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-07 00:10:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ async function startServer() {
     // TODO: USE aditional libraties, middlewares!
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
+    app.use(cors());
     app.use(router);
     app.listen('3030', () => console.log('Server operational on port: 3030!'));
 };
