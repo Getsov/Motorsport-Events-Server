3b022ab5ed8f81b45b0a3257618a287749232243
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-07 16:10:49 +0200

    Update userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -6,10 +6,9 @@ const ObjectId = mongoose.Types.ObjectId;
 
 //TODO: use env and change secret
 const secret = "q213fdfsddfasd231adfas12321kl";
-const password = "123";
-async function registerUser() {
-  /*
-    TODO: use next line for real app without hardcore the user
+// const password = "123";
+// async function registerUser() {
+
 
     async function registerUser(email, firstName, lastName,role,city, address, password) {
 
@@ -22,38 +21,34 @@ async function registerUser() {
             email,
             firstName,
             lastName,
-            role,
             city,
             address,
-            //TODO: fix likedEvents
-            likedEvents,
             hashedPassword: await bcrypt.hash(password, 10)
         });
         return createToken(user)
 
-*/
 
   // TODO: remove hardcore user
   // Sample ObjectId values that reference actual Event documents
-  const eventId1 = new ObjectId();
-  const eventId2 = new ObjectId();
-  const eventId3 = new ObjectId();
-  const user = await User.create({
-    email: "Shumaher@gmail.com",
-    firstName: "Michael",
-    lastName: "Schumaher",
-    role: "Racer",
-    city: "Sofia",
-    address: "Banishora 22",
-    likedEvents: [eventId1,eventId2,eventId3],
-    hashedPassword: await bcrypt.hash(password, 10),
-    isDeleted: false,
-  });
-
-  return user;
+  // const eventId1 = new ObjectId();
+  // const eventId2 = new ObjectId();
+  // const eventId3 = new ObjectId();
+  // const user = await User.create({
+  //   email: "Shumaher@gmail.com",
+  //   firstName: "Michael",
+  //   lastName: "Schumaher",
+  //   role: "Racer",
+  //   city: "Sofia",
+  //   address: "Banishora 22",
+  //   likedEvents: [eventId1,eventId2,eventId3],
+  //   hashedPassword: await bcrypt.hash(password, 10),
+  //   isDeleted: false,
+    
+  // });
+  // return user;
 }
 
-//TODO: With username or with email user will login into the app? Change appropriate
+
 async function loginUser(email, password) {
   const user = await User.findOne({ email });
   //TODO: check for isDeleted property
