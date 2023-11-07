f67da9f6fb98f5c3a559d56badbfeb4dbb13bd18
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-07 16:21:26 +0200

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,8 +4,6 @@ const { registerUser, loginUser } = require('../services/userService');
 //TODO:  In Future Pavka! Check CORS for requests "req" authorization, in index.js wich is commented!!!
 //TODO: In other words, if you turn on cors in index.js, no file will be returned on the client!
 
-
-//TODO: Change request from get => post
 userController.post('/registerUser', async (req, res) => {
     try {
         const user = await registerUser(
@@ -24,7 +22,6 @@ userController.post('/registerUser', async (req, res) => {
 });
 
 userController.post('/loginUser', async (req, res) => {
-    // TODO: Check for passwords and token..
     try {
         const user = await loginUser(req.body.email, req.body.password);
         res.status(200).json(user);
