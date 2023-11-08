eebac753d3f3f8c585e0c5999a6af071147243fd
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-08 16:54:08 +0200

    Update organizationService.js

--- src/services/organizationService.js
+++ src/services/organizationService.js
@@ -8,7 +8,7 @@ const secret = "q213fdfsddfasd231adfas12321kl";
 async function registerOrganization(organizationData) {
   const email = organizationData.email;
 
-  const existing = await User.findOne({ email });
+  const existing = await Organization.findOne({ email });
   if (existing) {
     throw new Error("Email is already taken!!!");
   }
@@ -16,64 +16,16 @@ async function registerOrganization(organizationData) {
   const organization = await Organization.create({
     name: organizationData.name,
     email: organizationData.email,
-    firstName: organizationData.managerFirstName,
-    lastName: organizationData.managerLastName,
-    password: organizationData.phone,
+    managerFirstName: organizationData.managerFirstName,
+    managerLastName: organizationData.managerLastName,
+    phone: organizationData.phone,
     region: organizationData.region,
-    address: organizationData.address ? organizationData.address : "",
-    hashedPassword: await bcrypt.hash(password, 10),
+    password: organizationData.phone,
+    address: organizationData.address ,
+    hashedPassword: await bcrypt.hash(organizationData.password, 10),
   });
-  return createToken(user);
+  return createToken(organization);
 }
-
-// TODO: remove hardcore organization & password
-// let password = 123;
-// const organization = await Organization.create({
-//   name: "Bardhal",
-//   email: "bardhal@gmail.com",
-//   city: "Sofiq",
-//   address: "Malinova Dolina 3",
-//   managerFirstName: "Ivan",
-//   managerLastName: "Ivan",
-//   phone: "0899123123",
-//   //This event is copied hardcore event from eventService.js
-//   //TODO: in created event array they key must be real event _id -> propertie must be the real event
-//   // createdEvents: [{"65463906dc185e1f50242d96": }
-//   // createdEvents: [
-//   //   {
-//   //     title: "Race Fanatic!",
-//   //     shortDescription: "ala bala",
-//   //     longDescription: "Lorem ipsum dolor sit amet.",
-//   //     dates: [
-//   //       {
-//   //         startDate: "2023-11-04T12:28:54.706Z",
-//   //         startTime: "23:59",
-//   //         endTime: "06:40",
-//   //         _id: "65463906dc185e1f50242d97",
-//   //       },
-//   //     ],
-//   //     imageUrl:
-//   //       "https://cdn.akamai.steamstatic.com/steam/apps/1839940/capsule_616x353.jpg?t=1683123302",
-//   //     contacts: {
-//   //       coordinates: { lat: "", long: "" },
-//   //       city: "Hisarya",
-//   //       address: "Хайдут Генчо N1",
-//   //       phone: "0123456789",
-//   //       email: "peter@abv.bg",
-//   //     },
-//   //     category: "Off Road",
-//   //     likedCount: 1,
-//   //     creator: "6542c24b6102c6f4e79108fc",
-//   //     winners: [{ name: "Az", _id: "65463906dc185e1f50242d98" }],
-//   //     isDeleted: false,
-//   //     _id: "65463906dc185e1f50242d96",
-//   //     __v: "0",
-//   //   },
-//   // ],
-//   isDeleted: false,
-//   hashedPassword: await bcrypt.hash(password, 10),
-// });
-// return organization;
 
 /*
 TODO:
@@ -81,6 +33,24 @@ TODO:
 - parseToken (can be exported)
 
 */
+
+function createToken(organization) {
+  const payload = {
+    _id: organization.id,
+    email: organization.email,
+    managerFirstName: organization.managerFirstName,
+    managerLastName: organization.lastName,
+    phone: organization.phone,
+  };
+  return {
+    _id: organization.id,
+    name: organization.name,
+    managerFirstName: organization.managerFirstName,
+    managerLastName: organization.lastName,
+    phone: organization.phone,
+    accessToken: jwt.sign(payload, secret),
+  };
+}
 
 module.exports = {
   registerOrganization,
