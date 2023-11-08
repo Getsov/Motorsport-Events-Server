7e5d1847f1f9ae35fdb72dc9ec8675e5a6e115f5
Hristo Yonkov <hristopturs@gmail.com>
2023-11-08 23:08:25 +0200

    Create limitModels.js

---
+++ src/utils/limitModels.js
@@ -0,0 +1,34 @@
+async function limitModels(model, page, limit) {
+    page = Number(page);
+    limit = Number(limit);
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const models = {};
+    console.log(page, limit);
+
+    if (!page && !limit) {
+        return await model.find();
+    }
+
+    if (endIndex < await model.countDocuments().exec()) {
+        models.nextPage = {
+            page: page + 1,
+            limit: limit
+        }
+    }
+
+    if (startIndex > 0) {
+        models.previousPage = {
+            page: page - 1,
+            limit: limit
+        }
+    }
+
+    models.results = await model.find().limit(limit).skip(startIndex).exec();
+    return models;
+}
+
+module.exports = {
+    limitModels
+}
