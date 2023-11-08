c9baee1e8f13f377919e1ca02ff1dc80dfcbe541
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-08 16:36:07 +0200

    Update organizationController.js

--- src/controllers/organizationController.js
+++ src/controllers/organizationController.js
@@ -1,8 +1,6 @@
 const organizationController = require('express').Router();
 const {registerOrganization} = require("../services/organizationService")
 
-//TODO : add organizationService 
-// const { registerUser, loginUser } = require('../services/userService');
 
 //TODO:  In Future Pavka! Check CORS for requests "req" authorization, in index.js wich is commented!!!
 //TODO: In other words, if you turn on cors in index.js, no file will be returned on the client!
@@ -20,9 +18,9 @@ organizationController.post('/registerOrganization', async (req, res) => {
             region: req.body.region,
             address: req.body.address ? req.body.address : "",
           };
-        const organization = await registerOrganization();
+        const organization = await registerOrganization(organizationData);
         
-        res.status(200).json(user);
+        res.status(200).json(organization);
         res.end();
     } catch (error) {
         res.status(400).json({ error: error.message });
