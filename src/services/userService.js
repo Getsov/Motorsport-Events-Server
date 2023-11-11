2bfcd0b578e098130cb881842bd18bf8660d42b1
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-11 14:32:47 +0200

    add updateUser functionality

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,9 +4,10 @@ const User = require("../models/User");
 
 //TODO: use env and change secret
 const secret = "q213fdfsddfasd231adfas12321kl";
+//TODO: Functionality for changing the password
 
-async function registerUser(userData) {
-  const email = userData.email;
+async function registerUser(requestBody) {
+  const email = requestBody.email;
   const existing = await User.findOne({ email });
   if (existing) {
     if (existing.isDeleted == true) {
@@ -17,11 +18,11 @@ async function registerUser(userData) {
   }
 
   const user = await User.create({
-    email: userData.email,
-    firstName: userData.firstName,
-    lastName: userData.lastName,
-    region: userData.region,
-    hashedPassword: await bcrypt.hash(userData.password, 10),
+    email: requestBody.email,
+    firstName: requestBody.firstName,
+    lastName: requestBody.lastName,
+    region: requestBody.region,
+    hashedPassword: await bcrypt.hash(requestBody.password, 10),
   });
   return createToken(user);
 }
@@ -43,10 +44,39 @@ async function loginUser(email, password) {
   return createToken(user);
 }
 
+async function getById(id) {
+  return User.find(id);
+}
+
+async function updateUser(tokenData, requestBody) {
+  const existing = await User.findById(tokenData._id);
+  /*TODO
+  if there is empty string in req.body- mongo
+  will kept the old record. 
+  Need one more check if field must be cleared
+  */
+ //TODO - check functionality with liked events
+  existing.email = requestBody.email ? requestBody.email : existing.email;
+  existing.firstName = requestBody.firstName
+    ? requestBody.firstName
+    : existing.firstName;
+  existing.lastName = requestBody.lastName
+    ? requestBody.lastName
+    : existing.lastName;
+  existing.region = requestBody.region 
+    ? requestBody.region 
+    : existing.region;
+  existing.hashedPassword = existing.hashedPassword;
+  const newRecord = await existing.save();
+  console.log(newRecord);
+  return createToken(newRecord);
+}
+
 function createToken(user) {
   //TODO: What payload will contain!
   const payload = {
     _id: user.id,
+    email: user.email,
     firstName: user.firstName,
     lastName: user.lastName,
     role: user.role,
@@ -78,4 +108,6 @@ module.exports = {
   registerUser,
   loginUser,
   parseToken,
+  getById,
+  updateUser,
 };
