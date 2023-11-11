b81ba2330c142186897df6f38f3bfd4d66989065
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-11 14:35:19 +0200

    Update session.js

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -2,6 +2,7 @@ const { parseToken } = require('../services/userService');
 
 module.exports = () => (req, res, next) => {
     const token = req.headers['x-authorization'];
+
     if (token) {
         try {
             const payload = parseToken(token);
