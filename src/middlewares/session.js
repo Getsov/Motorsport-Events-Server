b8b6523537914418d52170c1c597bab597e3c426
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-11 10:50:37 +0200

    change user => requester in sesion fn

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -2,11 +2,10 @@ const { parseToken } = require('../services/userService');
 
 module.exports = () => (req, res, next) => {
     const token = req.headers['x-authorization'];
-
     if (token) {
         try {
             const payload = parseToken(token);
-            req.user = payload;
+            req.requester = payload;
             req.token = token;
         } catch (error) {
             return res.status(401).json({ message: 'Invalid authorization token' })
