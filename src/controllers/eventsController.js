94ed96341040b49c64439c67e99de5cc191df01e
Hristo Yonkov <hristopturs@gmail.com>
2023-11-12 22:18:13 +0200

    add admin parameter

--- src/controllers/eventsController.js
+++ src/controllers/eventsController.js
@@ -66,7 +66,7 @@ eventController.put('/:id', async (req, res) => {
             throw new Error('You are not owner or Admin to modify this Event!');
         }
 
-        const updatedEvent = await updateEvent(req.body, event);
+        const updatedEvent = await updateEvent(req.body, event, req.requester.role === 'admin');
 
         res.status(200).json(updatedEvent);
         res.end();
