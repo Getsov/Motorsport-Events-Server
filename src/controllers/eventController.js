8b4ad23ac114b7f4459e42fb3f65f03cc4135a4e
Hristo Yonkov <hristopturs@gmail.com>
2023-11-15 22:10:47 +0200

    add check for isAdmin property.

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -64,15 +64,14 @@ eventController.put('/:id', async (req, res) => {
     try {
         const event = await findEventByID(req.params.id);
 
-        if (event === null || (req.requester.role !== 'admin' && event.isDeleted !== false)) {
+        if (event === null || (req.requester?.role !== 'admin' && event.isDeleted !== false)) {
             throw new Error('Event is deleted, or doesn\'t exist!');
         }
-
-        if (req.requester._id === undefined || (req.requester._id != event.creator && req.requester.role !== 'admin')) {
+        if (req.requester?._id === undefined || (req.requester._id != event.creator && req.requester.role !== 'admin')) {
             throw new Error('You are not owner or Admin to modify this Event!');
         }
 
-        const updatedEvent = await updateEvent(req.body, event);
+        const updatedEvent = await updateEvent(req.body, event, req.requester.role === 'admin');
 
         res.status(200).json(updatedEvent);
         res.end();
