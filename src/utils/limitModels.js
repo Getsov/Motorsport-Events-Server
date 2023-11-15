b718b57ba9491b03a2f0984616665a97063b5602
Hristo Yonkov <hristopturs@gmail.com>
2023-11-15 16:04:48 +0200

    find without isDeleted for request with all events and pagination done.

--- src/utils/limitModels.js
+++ src/utils/limitModels.js
@@ -7,10 +7,10 @@ async function limitModels(model, page, limit) {
     const models = {};
 
     if (!page && !limit) {
-        return await model.find();
+        return await model.find({ isDeleted: false });
     }
 
-    if (endIndex < await model.countDocuments().exec()) {
+    if (endIndex < await model.countDocuments({ isDeleted: false }).exec()) {
         models.nextPage = {
             page: page + 1,
             limit: limit
@@ -24,7 +24,7 @@ async function limitModels(model, page, limit) {
         }
     }
 
-    models.results = await model.find().limit(limit).skip(startIndex).exec();
+    models.results = await model.find({ isDeleted: false }).limit(limit).skip(startIndex).exec();
     return models;
 }
 
