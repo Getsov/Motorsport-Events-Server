c0019e30e9d555a904350d8cad83a79891f8491d
Hristo Yonkov <hristopturs@gmail.com>
2023-11-17 12:58:42 +0200

    Add comented code for postman testing purposes.

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -82,3 +82,69 @@ module.exports = {
     findEventsByCategory,
     likeUnlikeEvent
 }
+
+// Commented code below is for postman tests!
+
+// {
+//     "shortTitle": "Hissar Coup",
+//     "longTitle": "",
+//     "shortDescription": "Lorem..",
+//     "longDescription": "Lorem ipsum dolor sit amet miimet.",
+//     "dates": [
+//         {
+//             "startDate": "2023-11-17",
+//             "startTime": "23:59",
+//             "endTime": "06:40"
+//         }
+//     ],
+//     "imageUrl": "",
+//     "contacts": {
+//         "coordinates": {
+//             "lat": "42.52911093579847",
+//             "long": "24.707900125838766"
+//         },
+//         "region": "Hisarya",
+//         "address": "Хайдут Генчо N1",
+//         "phone": "0123456789",
+//         "email": "peter@abv.bg"
+//     },
+//     "category": "Drag",
+//     "likes": [
+//         "6542c24b6102c6f4e79108fc",
+//         "6542c24b6102c6f4e79108fc",
+//         "6542c24b6102c6f4e79108fc"
+//     ],
+//     "creator": "6550b41ee542ffd1875e2d38",
+//     "winners": [
+//         {
+//             "name": "Pavel",
+//             "vehicle": "Trabant",
+//             "place": 1,
+//             "_id": "65573f3a1bba3e1d78877f48"
+//         },
+//         {
+//             "name": "Ivan",
+//             "vehicle": "Wartburg",
+//             "place": 2,
+//             "_id": "65573f3a1bba3e1d78877f49"
+//         },
+//         {
+//             "name": "Dragan",
+//             "vehicle": "Moskvich",
+//             "place": 3,
+//             "_id": "65573f3a1bba3e1d78877f4a"
+//         }
+//     ],
+//     "visitorPrices": [
+//         {
+//             "price": 15,
+//             "description": "Цена за зрители"
+//         }
+//     ],
+//     "participantPrices": [
+//         {
+//             "price": 15,
+//             "description": "Цена за участници"
+//         }
+//     ]
+// }
