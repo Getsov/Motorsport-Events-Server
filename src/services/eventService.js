e7a4bd4d759d75ec96f8a2cca217cc44ac0cba1c
Hristo Yonkov <hristopturs@gmail.com>
2023-11-17 12:55:00 +0200

    update request to use requestBody

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,35 +1,22 @@
 const Event = require('../models/Event');
 const { limitModels } = require('../utils/limitModels');
 
-async function registerEvent() {
+async function registerEvent(requestBody) {
     // TODO: make more tests with different values!
     const event = await Event.create({
-        shortTitle: 'Race Fanatic',
-        longTitle: 'Race Fanatic Race',
-        shortDescription: 'ala bala',
-        longDescription: 'Lorem ipsum dolor sit amet.',
-        dates: [{
-            startDate: Date.now(),
-            startTime: '23:59',
-            endTime: '06:40'
-        }],
-        imageUrl: 'https://cdn.akamai.steamstatic.com/steam/apps/1839940/capsule_616x353.jpg?t=1683123302',
-        contacts: {
-            coordinates: { lat: '42.52911093579847', long: '24.707900125838766' },
-            region: 'Hisarya',
-            address: 'Хайдут Генчо N1',
-            phone: '0123456789',
-            email: 'peter@abv.bg'
-        },
-        category: 'Drag',
-        likes: ['6542c24b6102c6f4e79108fc', '6542c24b6102c6f4e79108fc', '6542c24b6102c6f4e79108fc'],
-        creator: '6550b41ee542ffd1875e2d38',
-        winners: [
-            { name: 'Pavel', vehicle: "Trabant", place: 1 },
-            { name: 'Ivan', vehicle: "Wartburg", place: 2 },
-            { name: 'Dragan', vehicle: "Moskvich", place: 3 }
-        ],
-        visitorPrices: [{ price: 15, description: 'Цена за зрители' }]
+        shortTitle: requestBody.shortTitle,
+        longTitle: requestBody.longTitle,
+        shortDescription: requestBody.shortDescription,
+        longDescription: requestBody.longDescription,
+        dates: requestBody.dates,
+        imageUrl: requestBody.imageUrl,
+        contacts: requestBody.contacts,
+        category: requestBody.category,
+        likes: requestBody.likes,
+        creator: requestBody.creator,
+        winners: requestBody.winners,
+        participantPrices: requestBody.participantPrices,
+        visitorPrices: requestBody.visitorPrices
     });
 
     return event;
