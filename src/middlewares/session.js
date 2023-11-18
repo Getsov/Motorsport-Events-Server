657614b4de2f9e26ac4c576bded5232e0016cfc2
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-18 13:23:53 +0200

    add TODO for future development

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -1,5 +1,6 @@
 const { parseToken } = require('../services/userService');
-
+//TODO - add session as middleware because 
+//organization and user use the same session function
 module.exports = () => (req, res, next) => {
     const token = req.headers['x-authorization'];
 
