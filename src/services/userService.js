f010a5fbb272552a0e820e3d46346fd2034c9f6c
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-25 16:09:44 +0200

    add TODO

--- src/services/userService.js
+++ src/services/userService.js
@@ -18,6 +18,7 @@ async function registerUser(requestBody) {
 
     const user = await User.create({
         email: requestBody.email,
+        //TODO - ask on meeting if is neseccery to make check for field organizatorName
         organizatorName: requestBody.organizatorName,
         firstName: requestBody.firstName,
         lastName: requestBody.lastName,
