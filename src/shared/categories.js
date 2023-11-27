adcc6480fbf579059596cc9c727d27aa1fb4c799
Hristo Yonkov <hristopturs@gmail.com>
2023-11-27 14:45:42 +0200

    Add categories

---
+++ src/shared/categories.js
@@ -0,0 +1,20 @@
+const categories = {
+    1: 'Drag',
+    2: 'Drift',
+    3: 'Time Attack',
+    4: 'Offroad',
+    5: 'Moto Race',
+    6: 'Motocross',
+    7: 'Hill Climb',
+    8: 'Track',
+    9: 'Rally',
+    10: 'Rally Sprint',
+    11: 'Rallycross',
+    12: 'Autocross',
+    13: 'Karting',
+    14: 'SIM Racing',
+    15: 'Събори'
+}
+module.exports = {
+    categories
+};
