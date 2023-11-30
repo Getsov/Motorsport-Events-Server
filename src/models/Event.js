af1e16e6cba98efa19a19ce5af508e45addf1f04
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-11-30 16:23:45 +0200

    add one more regex (optional)

--- src/models/Event.js
+++ src/models/Event.js
@@ -4,6 +4,7 @@ const {
     Types: { ObjectId },
 } = require('mongoose');
 const { phoneRegex } = require('../utils/sharedRegex');
+// const phoneNumberRegex = /^(\+\d{1,2}\s?)?(\(\d{1,4}\)\s?)?[\d\s.-]{5,}$/;
 const validTime = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
 const validUrl = /https?:\/\/./i;
 
