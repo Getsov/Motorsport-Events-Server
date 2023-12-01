b86031d8641376d2ef5f460d375f88afe4186af4
Hristo Yonkov <hristopturs@gmail.com>
2023-12-01 11:45:46 +0200

    removed "edndDate" and changed the variagle

--- src/models/Event.js
+++ src/models/Event.js
@@ -37,7 +37,7 @@ const eventSchema = new Schema({
     dates: {
         type: [
             {
-                startDate: {
+                date: {
                     type: Date,
                     required: true,
                     validate: {
@@ -65,18 +65,7 @@ const eventSchema = new Schema({
                         validator: (value) => validTime.test(value),
                         message: 'Invalid time format! Example: hh:mm (24h)',
                     },
-                },
-                endDate: {
-                    type: Date,
-                    // required: true,
-                    validate: {
-                        validator: function (date) {
-                            return date >= this.startDate;
-                        },
-                        message:
-                            'End date must be equal or greater than start date!',
-                    },
-                },
+                }
             },
         ],
     },
