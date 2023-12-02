6210318ec063c7d0adcb021a7e2494f89861d416
Hristo Yonkov <hristopturs@gmail.com>
2023-12-02 14:08:18 +0200

    Create sharedRegex.js

---
+++ src/shared/sharedRegex.js
@@ -0,0 +1,19 @@
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+const phoneRegex = /^[\d+()-\s]+$/;
+const validTime = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+const validUrl = /https?:\/\/./i;
+// const phoneNumberRegex = /^(\+\d{1,2}\s?)?(\(\d{1,4}\)\s?)?[\d\s.-]{5,}$/;
+
+
+// const checkPhoneNumber = (phoneNumber) => {
+//     const regexObj = new RegExp(phoneRegex);
+//     return regexObj.test(phoneNumber);
+// };
+
+module.exports = {
+    emailRegex,
+    phoneRegex,
+    validTime,
+    validUrl,
+    // checkPhoneNumber,
+};
