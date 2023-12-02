d003c682cf77bd2aebbbbab0acce0e44af44244d
Hristo Yonkov <hristopturs@gmail.com>
2023-12-02 17:15:02 +0200

    Update sharedRegex.js

--- src/shared/sharedRegex.js
+++ src/shared/sharedRegex.js
@@ -2,6 +2,7 @@ const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
 const phoneRegex = /^[\d+()-\s]+$/;
 const validTime = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
 const validUrl = /https?:\/\/./i;
+const validPassword = /^\S+$/;
 // const phoneNumberRegex = /^(\+\d{1,2}\s?)?(\(\d{1,4}\)\s?)?[\d\s.-]{5,}$/;
 
 
