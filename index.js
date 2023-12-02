06e796ed07661aab93f9f1c3c180ed32d9030e6f
Hristo Yonkov <hristopturs@gmail.com>
2023-12-02 18:28:47 +0200

    trimBody function commented

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function startServer() {
     app.use(cors());
     app.use(session());
     // TODO: Check about trim Body is viable solution?
-    app.use(trimBoddy());
+    // app.use(trimBoddy());
     app.use(router);
     app.listen('3030', () => console.log('Server operational on port: 3030!'));
 }
