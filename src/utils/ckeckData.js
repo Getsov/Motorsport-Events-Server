fd98d01dd71ad0d88035cdd5f2879fa8f5417882
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-06 17:43:31 +0200

    create functionality to escape xss attacks

---
+++ src/utils/ckeckData.js
@@ -0,0 +1,13 @@
+const { xssRegex } = require("../shared/sharedRegex");
+
+function checkRequestData(requestBody) {
+    for (const [key, value] of Object.entries(requestBody)) {
+        if (xssRegex.test(key) || xssRegex.test(value)) {
+            throw new Error('Data contains forbidden characters!');
+
+        }
+      }
+
+}
+
+module.exports = { checkRequestData };
