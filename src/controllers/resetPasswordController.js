761aae55c9307f88aab8a8fabd601e1ffe9aff86
Hristo Yonkov <hristopturs@gmail.com>
2023-12-08 15:40:05 +0200

    request for email message added!

--- src/controllers/resetPasswordController.js
+++ src/controllers/resetPasswordController.js
@@ -1,6 +1,35 @@
 const resetPasswordController = require('express').Router();
+const nodemailer = require('nodemailer');
 
 
+resetPasswordController.post('/', async (req, res) => {
+    try {
+        const { to, subject, text } = req.body;
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'racefanatictest@gmail.com',
+                pass: 'racefanatic123',
+            },
+        });
+
+        const mailOptions = {
+            from: 'racefanatictest@gmail.com',
+            to,
+            subject,
+            text,
+        };
+
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Email sent: ' + info.response);
+
+        res.status(200).send('Email sent successfully');
+        res.end();
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
 
 module.exports = {
     resetPasswordController,
