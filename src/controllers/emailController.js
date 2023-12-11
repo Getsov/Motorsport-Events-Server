d3b79b7d4182ad362fca6f07b333907309e53cff
Hristo Yonkov <hristopturs@gmail.com>
2023-12-11 11:47:51 +0200

    Name of the controller changed.

---
+++ src/controllers/emailController.js
@@ -0,0 +1,36 @@
+const resetPasswordController = require('express').Router();
+const nodemailer = require('nodemailer');
+
+
+resetPasswordController.post('/', async (req, res) => {
+    try {
+        const { to, subject, text } = req.body;
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'hristopturs@gmail.com',
+                pass: 'tkru yptn wijp ivly',
+            },
+        });
+
+        const mailOptions = {
+            from: 'hristopturs@gmail.com',
+            to,
+            subject,
+            text
+        };
+
+        const info = await transporter.sendMail(mailOptions);
+
+        console.log('Email sent: ' + info.response);
+        res.status(200).send('Email sent successfully');
+        res.end();
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+module.exports = {
+    resetPasswordController,
+};
