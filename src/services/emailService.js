eac8eab9ea4173986b7c8e565a49923aef029497
Hristo Yonkov <hristopturs@gmail.com>
2023-12-11 22:03:40 +0200

    add dependencies to work and comment about user

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,7 +1,14 @@
 const nodemailer = require('nodemailer');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const { secret } = require('../utils/parseToken');
 
 async function resetPassword(requestBody) {
     // TODO: Try to update the function later, with expired password or magic link..
+    const existingUser = await User.findOne({ email: requestBody.to });
+    //TODO: Create user and find it!
+    console.log(existingUser); 
 
     const { to, subject, text } = requestBody;
     console.log(to, subject, text);
