67ce3008fe10ec22063d1363bbdac529d095857f
Hristo Yonkov <hristopturs@gmail.com>
2023-12-11 16:02:19 +0200

    Create emailService.js

---
+++ src/services/emailService.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer');
+
+
+async function resetPassword() {
+    try {
+        const { to, subject, text } = req.body;
+
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'hristopturs@gmail.com',
+                pass: process.env.SENDER_EMAIL_PASSWORD,
+            },
+        });
+
+        const mailOptions = {
+            from: 'hristopturs@gmail.com',
+            to,
+            subject,
+            text
+        };
+
+        const info = await transporter.sendMail(mailOptions);
+
+        // console.log('Email sent: ' + info.response);
+        res.status(200).send('Email sent successfully');
+        res.end();
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+module.exports = {
+    resetPassword,
+};
