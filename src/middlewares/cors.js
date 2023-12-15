4154d1ff7d0b9381560be24cccec67daac70c402
Hristo Yonkov <hristopturs@gmail.com>
2023-12-15 17:07:27 +0200

    Update cors.js

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -2,7 +2,7 @@ module.exports = () => (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
         'Access-Control-Allow-Methods',
-        'HEAD, OPTIONS,GET,POST,PUT,DELETE'
+        'HEAD, OPTIONS, GET, POST, PUT, DELETE'
     );
     res.setHeader(
         'Access-Control-Allow-Headers',
