bc5a436be85297b9d44cefff18c00f60e0d93bcb
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-18 15:59:54 +0200

    remove logs

--- src/utils/checkDatesAndTime.js
+++ src/utils/checkDatesAndTime.js
@@ -3,15 +3,11 @@ function checkDatesAndTime(dates) {
         const currentDate = dates[i].date;
         const startTime = dates[i].startTime;
         const endTime = dates[i].endTime;
-        console.log(currentDate + 'currentDate');
-        console.log(startTime);
-        console.log(endTime);
         if (startTime >= endTime) {
             throw new Error("Start-time can't be after or equal to end-time!");
         }
         for (let j = 1; j < dates.length; j++) {
             const nextDate = dates[j].date;
-            console.log(nextDate + 'nextDate');
             if (currentDate === nextDate) {
                 throw new Error('Event cant be created with same dates!');
             }
