4db2da115847c6858bce7297eebb0ddaf1c31d75
Hristo Yonkov <hristopturs@gmail.com>
2023-12-23 23:35:38 +0200

    Update checkDatesAndTime.js

--- src/utils/checkDatesAndTime.js
+++ src/utils/checkDatesAndTime.js
@@ -1,14 +1,15 @@
 function checkDatesAndTime(dates) {
-    for (let i = 0; i < dates.length - 1; i++) {
+    for (let i = 0; i < dates.length; i++) {
         const currentDate = dates[i].date;
         const startTime = dates[i].startTime;
         const endTime = dates[i].endTime;
         if (startTime >= endTime) {
             throw new Error("Start-time can't be after or equal to end-time!");
         }
-        for (let j = 1; j < dates.length; j++) {
+        for (let j = i + 1; j < dates.length; j++) {
             const nextDate = dates[j].date;
             if (currentDate === nextDate) {
+                console.log(dates[i].date, dates[j].date);
                 throw new Error('Event cant be created with same dates!');
             }
         }
