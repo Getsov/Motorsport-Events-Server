584b291f5ee64b2059cdeadf71d620f23970b5fb
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-23 23:58:52 +0200

    add moch code in securityCheck function

---
+++ src/utils/securityCheck.js
@@ -0,0 +1,22 @@
+export async function checkAuthorityRequests(idOfUserForEdit, requesterId){
+// TODO - Export most of check from userService
+// function for approving from Valeri Getsov
+
+const userForEdit = await User.findById(idOfUserForEdit);
+const requester = await User.findById(requesterId);
+const isAdmin = requester.role == 'admin' ? true : false;
+
+if (!isAdmin && requester._id != idOfUserForEdit) {
+    throw new Error('You do not have rights to modify the record!');
+}
+if (!userForEdit || !requester) {
+    throw new Error('User not found');
+}
+if (userForEdit.isDeleted || requester.isDeleted) {
+    throw new Error('This profile is deleted!');
+}
+if (!userForEdit.isApproved || !requester.isApproved) {
+    throw new Error('This profile is not approved!');
+}
+
+}
