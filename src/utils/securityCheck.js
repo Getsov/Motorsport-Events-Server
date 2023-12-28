5f4d3dccd459fad04cc59b14706d2027d9852d0f
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-28 13:52:28 +0200

    refactor securityCheck file
    add property for admin request and not admin requests

--- src/utils/securityCheck.js
+++ src/utils/securityCheck.js
@@ -1,19 +1,38 @@
 module.exports.checkAuthorizedRequests = async function (
     userForEdit,
-    requester
+    requester,
+    isAdminRequest
 ) {
     const isAdmin = requester.role == 'admin' ? true : false;
 
-    if (!isAdmin && requester._id.toString() != userForEdit._id.toString()) {
-        throw new Error('You do not have rights to modify the record!');
-    }
-    if (!userForEdit || !requester) {
-        throw new Error('User not found');
-    }
-    if (userForEdit.isDeleted || requester.isDeleted) {
-        throw new Error('This profile is deleted!');
-    }
-    if (!userForEdit.isApproved || !requester.isApproved) {
-        throw new Error('This profile is not approved!');
+    if (isAdminRequest) {
+        if (!isAdmin) {
+            throw new Error('You do not have rights to modify the record!');
+        }
+        if (!userForEdit || !requester) {
+            throw new Error('User not found');
+        }
+        if (userForEdit.isDeleted || requester.isDeleted) {
+            throw new Error('This profile is deleted!');
+        }
+        if (!userForEdit.isApproved || !requester.isApproved) {
+            throw new Error('This profile is not approved!');
+        }
+    } else {
+        if (
+            !isAdmin &&
+            requester._id.toString() != userForEdit._id.toString()
+        ) {
+            throw new Error('You do not have rights to modify the record!');
+        }
+        if (!userForEdit || !requester) {
+            throw new Error('User not found');
+        }
+        if (userForEdit.isDeleted || requester.isDeleted) {
+            throw new Error('This profile is deleted!');
+        }
+        if (!userForEdit.isApproved || !requester.isApproved) {
+            throw new Error('This profile is not approved!');
+        }
     }
 };
