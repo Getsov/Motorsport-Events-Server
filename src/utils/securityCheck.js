627ed700b1723e6d5a809831beb2917931747454
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-28 11:34:09 +0200

    refactor securityCheck fn

--- src/utils/securityCheck.js
+++ src/utils/securityCheck.js
@@ -1,12 +1,10 @@
-async function checkAuthorizedRequests(idOfUserForEdit, requesterId) {
-    // TODO - Export most of check from userService
-    // function for approving from Valeri Getsov
-
-    const userForEdit = await User.findById(idOfUserForEdit);
-    const requester = await User.findById(requesterId);
+module.exports.checkAuthorizedRequests = async function (
+    userForEdit,
+    requester
+) {
     const isAdmin = requester.role == 'admin' ? true : false;
 
-    if (!isAdmin && requester._id != idOfUserForEdit) {
+    if (!isAdmin && requester._id != userForEdit._id) {
         throw new Error('You do not have rights to modify the record!');
     }
     if (!userForEdit || !requester) {
@@ -18,7 +16,4 @@ async function checkAuthorizedRequests(idOfUserForEdit, requesterId) {
     if (!userForEdit.isApproved || !requester.isApproved) {
         throw new Error('This profile is not approved!');
     }
-}
-module.exports = {
-    checkAuthorizedRequests,
 };
