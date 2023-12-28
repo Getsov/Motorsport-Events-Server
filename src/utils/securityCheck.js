a359cc78d00c19ca53fab65ff3c3c2cf0696ea21
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-28 11:12:53 +0200

    rename function

--- src/utils/securityCheck.js
+++ src/utils/securityCheck.js
@@ -1,22 +1,24 @@
-export async function checkAuthorityRequests(idOfUserForEdit, requesterId){
-// TODO - Export most of check from userService
-// function for approving from Valeri Getsov
+async function checkAuthorizedRequests(idOfUserForEdit, requesterId) {
+    // TODO - Export most of check from userService
+    // function for approving from Valeri Getsov
 
-const userForEdit = await User.findById(idOfUserForEdit);
-const requester = await User.findById(requesterId);
-const isAdmin = requester.role == 'admin' ? true : false;
+    const userForEdit = await User.findById(idOfUserForEdit);
+    const requester = await User.findById(requesterId);
+    const isAdmin = requester.role == 'admin' ? true : false;
 
-if (!isAdmin && requester._id != idOfUserForEdit) {
-    throw new Error('You do not have rights to modify the record!');
+    if (!isAdmin && requester._id != idOfUserForEdit) {
+        throw new Error('You do not have rights to modify the record!');
+    }
+    if (!userForEdit || !requester) {
+        throw new Error('User not found');
+    }
+    if (userForEdit.isDeleted || requester.isDeleted) {
+        throw new Error('This profile is deleted!');
+    }
+    if (!userForEdit.isApproved || !requester.isApproved) {
+        throw new Error('This profile is not approved!');
+    }
 }
-if (!userForEdit || !requester) {
-    throw new Error('User not found');
-}
-if (userForEdit.isDeleted || requester.isDeleted) {
-    throw new Error('This profile is deleted!');
-}
-if (!userForEdit.isApproved || !requester.isApproved) {
-    throw new Error('This profile is not approved!');
-}
-
-}
+module.exports = {
+    checkAuthorizedRequests,
+};
