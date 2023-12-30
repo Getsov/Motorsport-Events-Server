0fb981ca6d2b32ea69b53f283339469a13585922
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-12-30 09:33:21 +0200

    refactor checkAuthorizedRequests Fn

--- src/utils/securityCheck.js
+++ src/utils/securityCheck.js
@@ -1,56 +1,55 @@
 module.exports.checkAuthorizedRequests = async function (
     userForEdit,
     requester,
-    isAdminRequest,
-    editDeleteRequest,
-    editApproveRequest
+    isAdmin
 ) {
-    const isAdmin = requester.role == 'admin' ? true : false;
-
-    if (isAdminRequest) {
-        if (!isAdmin) {
-            throw new Error('You do not have rights to modify the record!');
-        }
-        if (!userForEdit || !requester) {
-            throw new Error('User not found');
-        }
-        if (editDeleteRequest) {
-            if (requester.isDeleted) {
-                throw new Error('Your profile is deleted!');
-            }
-            if (!requester.isApproved) {
-                throw new Error('Your profile is not approved!');
-            }
-        } else if (!editDeleteRequest && editApproveRequest) {
-            if (userForEdit.isDeleted || requester.isDeleted) {
-                throw new Error('This profile is deleted!');
-            }
-            if (!requester.isApproved) {
-                throw new Error('Your profile is not approved!');
-            }
-        } else {
-            if (userForEdit.isDeleted || requester.isDeleted) {
-                throw new Error('This profile is deleted!');
-            }
-            if (!userForEdit.isApproved || !requester.isApproved) {
-                throw new Error('This profile is not approved!');
-            }
-        }
-    } else {
-        if (
-            !isAdmin &&
-            requester._id.toString() != userForEdit._id.toString()
-        ) {
-            throw new Error('You do not have rights to modify the record!');
-        }
-        if (!userForEdit || !requester) {
-            throw new Error('User not found');
-        }
-        if (userForEdit.isDeleted || requester.isDeleted) {
-            throw new Error('This profile is deleted!');
-        }
-        if (!userForEdit.isApproved || !requester.isApproved) {
-            throw new Error('This profile is not approved!');
-        }
+    if (requester.isDeleted) {
+        throw new Error('Your profile is deleted!');
     }
+
+    if (!requester.isApproved) {
+        throw new Error('Your profile is not approved!');
+    }
+
+    if (!isAdmin && requester._id.toString() != userForEdit._id.toString()) {
+        throw new Error('You do not have rights to modify the record!');
+    }
+
+    if (!userForEdit || !requester) {
+        throw new Error('User not found');
+    }
+
+    if (userForEdit.isDeleted) {
+        throw new Error('This profile is deleted!');
+    }
+
+    if (!userForEdit.isApproved) {
+        throw new Error('This profile is not approved!');
+    }
+
+    // if (isAdminRequest) {
+    //     if (!isAdmin) {
+    //         throw new Error('You do not have rights to modify the record!');
+    //     }
+    //     if (!userForEdit || !requester) {
+    //         throw new Error('User not found');
+    //     }
+    //     if (editDeleteRequest) {
+
+    //     } else if (!editDeleteRequest && editApproveRequest) {
+    //         if (userForEdit.isDeleted ) {
+    //             throw new Error('This profile is deleted!');
+    //         }
+
+    //     } else {
+    //         if (userForEdit.isDeleted || requester.isDeleted) {
+    //             throw new Error('This profile is deleted!');
+    //         }
+    //         if (!userForEdit.isApproved || !requester.isApproved) {
+    //             throw new Error('This profile is not approved!');
+    //         }
+    //     }
+    // } else {
+
+    // }
 };
