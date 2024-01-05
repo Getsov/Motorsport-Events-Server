544471e619f5100058feba769cc2b24851ba4e29
Hristo Yonkov <hristopturs@gmail.com>
2024-01-05 11:41:49 +0200

    Fixed the mistake.

--- src/services/userService.js
+++ src/services/userService.js
@@ -166,12 +166,12 @@ async function editDeletedProperty(idOfUserForEdit, requestBody, requesterId) {
   const requester = await User.findById(requesterId);
   const isAdmin = requester.role == 'admin' ? true : false;
 
-  if (!isAdmin || requester.isDeleted || !requester.isApproved) {
-    throw new Error('You do not have rights to modify the record!');
-  }
-
   if (!userForEdit) {
     throw new Error('User not found');
+  }
+
+  if (!isAdmin || requester.isDeleted || !requester.isApproved) {
+    throw new Error('You do not have rights to modify the record!');
   }
 
   if (requestBody.hasOwnProperty('isDeleted')) {
