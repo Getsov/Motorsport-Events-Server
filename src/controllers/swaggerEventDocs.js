c5f4c7884ca6888863e8bbec153ec76568339a80
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-08 23:00:37 +0200

    Update swaggerEventDocs.js

--- src/controllers/swaggerEventDocs.js
+++ src/controllers/swaggerEventDocs.js
@@ -1,6 +1,6 @@
 /**
  * @swagger
- * /event:
+ * /event/register:
  *   post:
  *     summary: Register Event.
  *     description: Create a new Event. If you register as admin, you need to pass "role". If you register as organizator you need to add to request role, organizatorName, phone. The service automatically creates a session and returns object with accessToken inside, that can be used for authorized requests. Admins and organizаtors will be active after approval by active admin.
