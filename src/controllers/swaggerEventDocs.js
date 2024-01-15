1afff416f8a8c0d100ac03c7569c31263e2fcd0c
Hristo Yonkov <hristopturs@gmail.com>
2024-01-15 13:23:25 +0200

    added event registration field in swagger

--- src/controllers/swaggerEventDocs.js
+++ src/controllers/swaggerEventDocs.js
@@ -1,35 +1,70 @@
 /**
  * @swagger
- * /event/register:
+ * /events/register:
  *   post:
+ *     parameters:
+ *       - name: x-authorization
+ *         in: header
+ *         description: JWT token needed for the request
+ *         required: true
+ *         type: string
  *     summary: Register Event.
- *     description: Create a new Event. If you register as admin, you need to pass "role". If you register as organizator you need to add to request role, organizatorName, phone. The service automatically creates a session and returns object with accessToken inside, that can be used for authorized requests. Admins and organizаtors will be active after approval by active admin.
+ *     description: Important!
+ *          Event must be approved to be seen from other users than owner and admin!
+ *          The shape of the body is restricted. 
+ *          The service will respond with the object, created in the DB, which will have an added
+ *          ` _id` property, that is automatically generated.
+ *          In this event example only needed fields are provided for creating it.
  *     tags:
  *       - Event
- *     parameters:
- *       - in: path
- *         name: email
- *         required: true
- *         description: email for registration
- *         schema:
- *         type: string
- *
- *       - in: path
- *         name: password
- *         required: true
- *         description: password for registration
- *         schema:
- *         type: string
- * 
- *       - in: path
- *         name: repassword
- *         required: true
- *         description: repetition of password
- *         schema:
- *         type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               shortTitle:
+ *                 type: string
+ *               shortDescription:
+ *                 type: string
+ *               dates:
+ *                 type: array
+ *               contacts:
+ *                 type: object
+ *               categories:
+ *                 type: array
+ *               visitorPrices:
+ *                 type: array
+ *             example:
+ *              shortTitle: Hissar Motocross
+ *              shortDescription: Motocross in Hissar..
+ *              dates: [{
+ *                  "date": "2024-12-25",
+ *                  "startTime": "10:59",
+ *                  "endTime": "22:40"
+ *              }]
+ *              contacts: {
+ *                  "coordinates": {
+ *                       "lat": "42.52911093579847",
+ *                       "lng": "24.707900125838766"
+ *                   },
+ *                  "region": "Пловдив",
+ *                  "address": "Хайдут Генчо N1",
+ *                  "phone": "0123456789",
+ *                  "email": "peter@abv.bg"
+ *              }
+ *              categories: ["Драг"]
+ *              visitorPrices: [
+ *                  {
+ *                      "price": 15,
+ *                      "description": "Цена за зрители"
+ *                  }
+ *              ]                       
  *     responses:
  *       200:
- *         description: Details of the user.
- *       404:
- *         description: User not found.
+ *         description: Details of the Event.
+ *       400:
+ *         description: Event not found.
  */
+
