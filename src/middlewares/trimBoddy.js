937b1556d43f4ad61c10514e2afe4ead0133942f
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-16 22:30:48 +0200

    remove trimBody fn - is not useful

--- src/middlewares/trimBoddy.js
+++
@@ -1,9 +0,0 @@
-module.exports = () => (req, res, next) => {
-  for (let key in req.body) {
-    if (typeof req.body[key] == 'string') {
-      req.body[key] = req.body[key].trim();
-    }
-  }
-
-  next();
-};
