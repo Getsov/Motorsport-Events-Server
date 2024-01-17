4637d50e7a5cd9720229cdcc950f01dfaf55f6b0
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-17 23:08:08 +0200

    format swaggerConfig

--- swaggerConfig.js
+++ swaggerConfig.js
@@ -6,7 +6,8 @@ const options = {
     info: {
       title: '"Race Fanatic" Documentation',
       version: '1.0.0',
-      description: 'This is a REST service, created for "Race Fanatic" APP.\
+      description:
+        'This is a REST service, created for "Race Fanatic" APP.\
       To properly work the server need installed mongoDB on local machine for database. Add the connection string in the `index.js` file.\
       To build the server, run the following commands in the terminal: `npm install`, `npm start`.\
       ',
@@ -14,21 +15,19 @@ const options = {
     components: {
       securitySchemes: {
         apiKey: {
-          type: "apiKey",
-          in: "header",
-          name: "x-authorization",
-          description: "Add JSON Web Token (JWT) for authentication."
-        }
-      }
+          type: 'apiKey',
+          in: 'header',
+          name: 'x-authorization',
+          description: 'Add JSON Web Token (JWT) for authentication.',
+        },
+      },
     },
-    security: [
-      { apiKey: [] }
-    ],
+    security: [{ apiKey: [] }],
     servers: [
       {
-        url: "http://localhost:3030/"
+        url: 'http://localhost:3030/',
       },
-    ]
+    ],
   },
   apis: ['./src/controllers/*.js'], // Path to the API routes
 };
