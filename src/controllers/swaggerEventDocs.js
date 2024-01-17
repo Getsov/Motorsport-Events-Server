60cad92bdca876fc15f0649915d9e2b597e68803
Hristo Yonkov <hristopturs@gmail.com>
2024-01-17 16:57:31 +0200

    Add edit route in swagger

--- src/controllers/swaggerEventDocs.js
+++ src/controllers/swaggerEventDocs.js
@@ -2,12 +2,7 @@
  * @swagger
  * /events/register:
  *   post:
- *     parameters:
- *       - name: x-authorization
- *         in: header
- *         description: JWT token needed for the request
- *         type: string
- *     summary: Register Event.
+ *     summary: Register Event
  *     description: Important!
  *          Event must be approved to be seen from other users than owner and admin!
  *          The shape of the body is restricted. 
@@ -53,7 +48,7 @@
  *                  "phone": "0123456789",
  *                  "email": "peter@abv.bg"
  *              }
- *              categories: ["Драг"]
+ *              categories: ["Драг", Дрифт]
  *              visitorPrices: [
  *                  {
  *                      "price": 15,
@@ -71,11 +66,6 @@
  * @swagger
  * /events/eventsForApproval:
  *   get:
- *     parameters:
- *       - name: x-authorization
- *         in: header
- *         description: JWT token needed for the request
- *         type: string
  *     summary: Events waiting for approval.
  *     description: Retrieve all `events` waiting for approval! Available only for active and approved admins!
  *     tags:
@@ -197,4 +187,72 @@
  *         description: Array of events or empty array if no events in the Database.
  *       400:
  *         description: Server Error.
+ */
+
+/**
+ * @swagger
+ * /events/{id}:
+ *   put:
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: Required `id` of the event to update
+ *         type: string
+ *     summary: Update Event
+ *     description: Important! Event here is not returned by the data!
+ *          In order to properly edit event check the data in hardcoded event.
+ *          The shape of the body is restricted. 
+ *          The service will respond with the object, edited in the DB,
+ *          In this event example only needed fields are provided for editing it.
+ *     tags:
+ *       - Event
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               shortTitle:
+ *                 type: string
+ *               shortDescription:
+ *                 type: string
+ *               dates:
+ *                 type: array
+ *               contacts:
+ *                 type: object
+ *               categories:
+ *                 type: array
+ *               visitorPrices:
+ *                 type: array
+ *             example:
+ *              shortTitle: Hissar Motocross
+ *              shortDescription: Motocross in Hissar..
+ *              dates: [{
+ *                  "date": "2024-12-25",
+ *                  "startTime": "10:59",
+ *                  "endTime": "22:40"
+ *              }]
+ *              contacts: {
+ *                  "coordinates": {
+ *                       "lat": "42.52911093579847",
+ *                       "lng": "24.707900125838766"
+ *                   },
+ *                  "region": "Пловдив",
+ *                  "address": "Хайдут Генчо N1",
+ *                  "phone": "0123456789",
+ *                  "email": "peter@abv.bg"
+ *              }
+ *              categories: ["Драг", Дрифт]
+ *              visitorPrices: [
+ *                  {
+ *                      "price": 15,
+ *                      "description": "Цена за зрители"
+ *                  }
+ *              ]                       
+ *     responses:
+ *       200:
+ *         description: Details of the Event.
+ *       400:
+ *         description: Event not found.
  */
