f049854e8e56dc3f5b51624b91c38a28cb8d8bfc
Hristo Yonkov <hristopturs@gmail.com>
2024-01-20 14:27:55 +0200

    changed check logic

--- src/utils/getNeededDates.js
+++ src/utils/getNeededDates.js
@@ -1,19 +1,19 @@
 function getNeededDates(year, month) {
-  if (typeof year != 'number' || typeof month != 'number') {
+  if (isNaN(year) || isNaN(month)) {
     throw new Error('Invalid querry. Year and month should be numbers.');
   }
-
+  
   if (year.toString().length != 4) {
     throw new Error('Invalid year.');
   }
-
+  
   if (month < 1 || month > 12) {
     throw new Error('Invalid month value. Month should be in the range 1-12.');
   }
-
+  
   const startDate = new Date(year, month - 1, 1);
   startDate.setHours(0, 0, 0, 0);
-
+  
   const endDate = new Date(year, month, 0);
   endDate.setHours(23, 59, 59, 999);
 
