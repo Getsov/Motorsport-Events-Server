61ff33a7f2e0aade24733f8ab6d1f5faec02306e
Hristo Yonkov <hristopturs@gmail.com>
2024-01-24 15:27:39 +0200

    add proper check for requesterId

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -358,7 +358,7 @@ userController.get('/allOrganizers', async (req, res) => {
 
 userController.get('/allRegularUsers', async (req, res) => {
   try {
-    const requesterId = req.requester._id;
+    const requesterId = req.requester?._id;
     const result = await getAllRegularUsers(requesterId);
     res.status(200).json(result);
     res.end();
