fdb2af18e6f71d307ab8d073efb1d9e14be633cf
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-25 23:22:58 +0200

    remove console.log()

--- src/utils/parseToken.js
+++ src/utils/parseToken.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const secret = process.env.AUTH_TOKEN_SECRET;
-console.log(secret);
 function parseToken(token) {
   try {
     return jwt.verify(token, secret);
