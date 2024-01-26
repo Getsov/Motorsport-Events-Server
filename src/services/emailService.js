a0f3f3d6accbfc9269b275e96475ec207a81e92b
Hristo Yonkov <hristopturs@gmail.com>
2024-01-26 22:17:24 +0200

    add new function for user sending email

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -43,6 +43,50 @@ async function resetPassword(requestBody) {
   return info;
 }
 
+async function sendWhenApproveDisapprove(usersList, isApproved) {
+  // TODO: Try to update the function later, with expired password or magic link..
+  // const existingUser = await User.findOne({ email: requestBody?.to });
+  // if (!existingUser) {
+  //   throw new Error('User-Email not found!');
+  // }
+
+  // const newPassword = generatePassword();
+
+  // existingUser.hashedPassword = await bcrypt.hash(newPassword, 10);
+
+  // await existingUser.save();
+
+  // const { to } = requestBody;
+  const to = [];
+
+  usersList.forEach(user => {
+    to.push(user.email)
+  });
+
+  const transporter = nodemailer.createTransport({
+    host: 'mail.racefanatic.app',
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: 'test@racefanatic.app', // your domain email address
+      pass: process.env.EMAIL_PASS, // your password
+    },
+  });
+
+  const mailOptions = {
+    from: 'admin@racefanatic.app',
+    to,
+    subject: 'Approved racefanatic profile',
+    text: `Your profile is approved in race fanatic app`,
+  };
+
+  const info = await transporter.sendMail(mailOptions);
+
+  console.log('Email sent: ' + info.response);
+  return info;
+}
+
 module.exports = {
   resetPassword,
+  sendWhenApproveDisapprove
 };
