1e9add4189bf60a0a55c2682ee73cb0c615abee9
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-28 23:18:38 +0200

    add cookie in headers

--- src/controllers/refreshTokenController.js
+++ src/controllers/refreshTokenController.js
@@ -4,18 +4,30 @@ const { parseRefreshToken } = require('../utils/parseToken');
 
 refreshTokenController.post('/', async (req, res) => {
   const refreshToken = req.cookies['refreshToken'];
-
+  console.log(refreshToken);
   if (!refreshToken) {
     return res.status(401).send('Access Denied. No refresh token provided.');
   }
-
   if (refreshToken) {
+    console.log('have refreshtoken');
     try {
+      console.log('before');
       const payload = parseRefreshToken(refreshToken);
+      console.log('after');
       const userId = payload._id;
       const tokens = await getUserForTokenGenereating(userId);
-      //   console.log(tokens);
-      res.status(200).json({ tokens });
+      const accessToken = tokens.accessToken;
+      const refreshToken = tokens.refreshToken;
+      console.log(tokens);
+      res
+        .status(200)
+        .cookie('refreshToken', refreshToken, {
+          httpOnly: true,
+          sameSite: 'strict',
+        })
+        // .header('Authorization', accessToken)
+        .send(accessToken);
+      res.end();
       res.end();
     } catch (error) {
       return res.status(401).json({ message: 'Invalid refresh token' });
