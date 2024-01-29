edc2d67e495d1e8d02b249139d79fd1ba76651bf
Valeri Getsov <info@vgetsov.com>
2024-01-29 19:50:04 +0200

    mongodb deploy to atlas

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('./src/middlewares/cors');
-const dbConnectionString = 'mongodb://127.0.0.1:27017/Race-Fanatic-DB';
-const { mongoose } = require('mongoose');
-const session = require('./src/middlewares/session');
-const router = require('./src/routes');
-mongoose.set('strictQuery', true);
-require('dotenv').config();
+const cors = require("./src/middlewares/cors");
+const { mongoose } = require("mongoose");
+const session = require("./src/middlewares/session");
+const router = require("./src/routes");
+mongoose.set("strictQuery", true);
+require("dotenv").config();
 
+const dbConnectionString = process.env.MONGO_URI;
 const initializeDatabase = () =>
   mongoose
     .connect(dbConnectionString)
     .then(() => {
-      console.log('Database connected!');
+      console.log("Database connected!");
     })
-    .catch(() => {
-      console.log('Database connection FAILED!');
+    .catch((err) => {
+      console.log(err);
+      console.log("Database connection FAILED!");
     });
 
 async function startServer() {
   initializeDatabase();
-  app.use(express.json({ limit: '10mb' }));
+  app.use(express.json({ limit: "10mb" }));
   app.use(express.urlencoded({ extended: false }));
   app.use(cors());
   app.use(session());
   app.use(router);
-  app.listen('3030', () => console.log('Server operational on port: 3030!'));
+  app.listen("3030", () => console.log("Server operational on port: 3030!"));
 }
 startServer();
