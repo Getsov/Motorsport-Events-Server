f57828074091196e7754155d16e55421290bc53c
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-31 00:01:34 +0200

    update with data from dev branch

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const cors = require('./src/middlewares/cors');
-const dbConnectionString = 'mongodb://127.0.0.1:27017/Race-Fanatic-DB';
+// const dbConnectionString = 'mongodb://127.0.0.1:27017/Race-Fanatic-DB';
 const { mongoose } = require('mongoose');
 const session = require('./src/middlewares/session');
 const router = require('./src/routes');
 mongoose.set('strictQuery', true);
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
+const dbConnectionString = process.env.MONGO_URI;
+
+// Development DB connection string
+// const dbConnectionString = 'mongodb://127.0.0.1:27017/Race-Fanatic-DB';
 const initializeDatabase = () =>
   mongoose
     .connect(dbConnectionString)
