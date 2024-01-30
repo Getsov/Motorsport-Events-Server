8c680dbf7418b2fc46c86f88f9cc872ae256c6c2
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-01-30 23:53:02 +0200

    remove logs and return propper object

--- src/controllers/refreshTokenController.js
+++ src/controllers/refreshTokenController.js
@@ -4,21 +4,16 @@ const { parseRefreshToken } = require('../utils/parseToken');
 
 refreshTokenController.post('/', async (req, res) => {
   const cookieRefreshToken = req.cookies['refreshToken'];
-  console.log(cookieRefreshToken);
   if (!cookieRefreshToken) {
     return res.status(401).send('Access Denied. No refresh token provided.');
   }
   if (cookieRefreshToken) {
-    console.log('have refreshtoken');
     try {
-      console.log('before');
       const payload = parseRefreshToken(cookieRefreshToken);
-      console.log('after');
       const userId = payload._id;
       const tokens = await getUserForTokenGenereating(userId);
       const accessToken = tokens.accessToken;
       const refreshToken = tokens.refreshToken;
-      console.log(tokens);
       res
         .status(200)
         .cookie('refreshToken', refreshToken, {
@@ -26,7 +21,7 @@ refreshTokenController.post('/', async (req, res) => {
           sameSite: 'strict',
         })
         // .header('Authorization', accessToken)
-        .send(accessToken);
+        .send({ accessToken });
       res.end();
       res.end();
     } catch (error) {
