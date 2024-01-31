8fa1f3509a090a42741c0670069e6c7b571fd4e4
Hristo Yonkov <hristopturs@gmail.com>
2024-01-31 17:32:51 +0200

    Update limitModels.js
    1. add check with functionality to search all events
    2. add check and functionality to search past events
    3. add check and functionality to search upcoming events

--- src/utils/limitModels.js
+++ src/utils/limitModels.js
@@ -2,33 +2,37 @@ async function limitModels(model, page, limit, criteria, sortCriteria) {
   page = Number(page);
   limit = Number(limit);
 
-  if (page === 0) page = 1;
-
   const startIndex = (page - 1) * limit;
   const endIndex = page * limit;
   const models = {};
+  
+  if (sortCriteria === 'allEvents') {
+    let todayStart = new Date(Date.now());
+    todayStart.setHours(0, 0, 0, 0);
 
-  if (!page && !limit) {
-    return await model.find(criteria);
-  }
-
-  if (endIndex < (await model.countDocuments(criteria).exec())) {
-    models.nextPage = {
-      page: page + 1,
-      limit: limit,
+    upcomingDates = {
+      $elemMatch: {
+        date: { $gte: todayStart },
+      },
     };
-  }
+    criteria.dates = upcomingDates;
 
-  if (startIndex > 0) {
-    models.previousPage = {
-      page: page - 1,
-      limit: limit,
+    let upcomingEvents = await model
+      .find(criteria)
+      .sort({ 'dates.0.date': 1 })
+      .exec();
+
+    pastDates = {
+      $not: {
+        $elemMatch: {
+          date: { $gte: todayStart },
+        },
+      },
     };
-  }
+    criteria.dates = pastDates;
 
-  if (sortCriteria === 'pastEvents') {
     const aggregationPipeline = [
-      { $match: criteria }, // Match documents based on criteria
+      { $match: criteria },
       {
         $addFields: {
           lastDate: {
@@ -40,26 +44,105 @@ async function limitModels(model, page, limit, criteria, sortCriteria) {
           },
         },
       },
-      { $sort: { lastDate: -1 } }, // Sort by the last date in descending order
-      { $skip: startIndex },
+      { $sort: { lastDate: -1 } },
+      { $project: { lastDate: 0 } },
     ];
 
-    if (limit > 0) {
-      aggregationPipeline.push({ $limit: limit });
+    let pastEvents = await model.aggregate(aggregationPipeline).exec();
+    const concatenatedEvents = upcomingEvents.concat(pastEvents);
+
+    if (endIndex < (concatenatedEvents.length) && limit) {
+      models.nextPage = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+    if (startIndex > 0) {
+      models.previousPage = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+    if (limit) {
+      models.results = concatenatedEvents.slice(startIndex, startIndex + limit);
+      return models;
+    }
+    models.results = concatenatedEvents;
+    return models;
+  }
+  
+  if (sortCriteria === 'pastEvents') {
+    const aggregationPipeline = [
+      { $match: criteria },
+      {
+        $addFields: {
+          lastDate: {
+            $cond: {
+              if: { $ne: ['$dates', []] },
+              then: { $arrayElemAt: ['$dates.date', -1] },
+              else: null,
+            },
+          },
+        },
+      },
+      { $sort: { lastDate: -1 } },
+      { $project: { lastDate: 0 } },
+    ];
+
+    const foundEvents = await model.aggregate(aggregationPipeline).exec();
+
+    if (endIndex < (foundEvents.length) && limit) {
+      models.nextPage = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+    
+    if (startIndex > 0) {
+      models.previousPage = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+    
+    if (limit) {
+      models.results = foundEvents.slice(startIndex, startIndex + limit);
+      return models;
     }
 
-    models.results = await model.aggregate(aggregationPipeline).exec();
+    models.results = foundEvents;
+    return models;
+  }
+  
+  if (sortCriteria === 'upcomingEvents') {
+    const foundEvents = await model
+    .find(criteria)
+    .sort({ 'dates.0.date': 1 })
+    .exec();
+    
+    if (endIndex < (foundEvents.length) && limit) {
+      models.nextPage = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+  
+    if (startIndex > 0) {
+      models.previousPage = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+  
+    if (limit) {
+      models.results = foundEvents.slice(startIndex, startIndex + limit);
+      return models;
+    }
+  
+    models.results = foundEvents;
     return models;
   }
 
-  models.results = await model
-    .find(criteria)
-    .sort(sortCriteria)
-    .limit(limit)
-    .skip(startIndex)
-    .exec();
-
-  return models;
 }
 
 module.exports = {
