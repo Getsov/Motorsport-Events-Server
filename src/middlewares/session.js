328e6c5a58200de1e6689d0240ee282af15ca9eb
Hristo Yonkov <hristopturs@gmail.com>
2024-02-22 15:58:12 +0200

    add checks and comment to discuss it with mates later

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -2,7 +2,10 @@ const { parseAccesToken } = require('../utils/parseToken');
 module.exports = () => (req, res, next) => {
   const token = req.headers['x-authorization'];
   const path = req.path;
+  console.log("TOKEN:", token);
+  // May be path must be: '/user/refreshToken'
   if (path != '/refreshToken') {
+    console.log('not equal');
     if (token) {
       try {
         const payload = parseAccesToken(token);
