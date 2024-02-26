77fdd20542ac9fa0856acb9ae52f088d30c145dc
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-02-26 23:12:15 +0200

    add descriptions in swaggerConfig file

--- swaggerConfig.js
+++ swaggerConfig.js
@@ -8,7 +8,7 @@ const options = {
       version: '1.0.0',
       description:
         'This is a REST service, created for "Race Fanatic" APP.\
-      To properly work the server need installed mongoDB on local machine for database. Add the connection string in the `index.js` file.\
+      To properly work the server needs MongoDB installed on the local machine for database. Add the connection string in the `index.js` file.\
       To build the server, run the following commands in the terminal: `npm install`, `npm start`.\
       ',
     },
@@ -25,8 +25,12 @@ const options = {
     security: [{ apiKey: [] }],
     servers: [
       {
-        // url: 'http://localhost:3030/',
+        url: 'http://localhost:3030/',
+        description: 'URL for tesing in the local environment.',
+      },
+      {
         url: 'https://race-fanatic-server.onrender.com/',
+        description: 'URL for production environment.',
       },
     ],
   },
