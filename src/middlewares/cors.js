11b7c51a4255dc5f52c8e99eb1b86cfdcd16c860
Pavel.Dimitrov <2heads.pd@gmail.com>
2024-03-03 23:31:43 +0200

    add comments and Allow credentials

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,5 +1,10 @@
 module.exports = () => (req, res, next) => {
+  
+ // Replace with actual frontend origin
   res.setHeader('Access-Control-Allow-Origin', '*');
+  
+  //Ionic default port
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
   res.setHeader(
     'Access-Control-Allow-Methods',
     'HEAD, OPTIONS, GET, POST, PUT, DELETE'
@@ -8,7 +13,8 @@ module.exports = () => (req, res, next) => {
     'Access-Control-Allow-Headers',
     'Content-Type, X-Authorization'
   );
-
+  // Allow credentials in CORS
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   if (req.method === 'OPTIONS') {
     res.status(200).end();
   } else {
